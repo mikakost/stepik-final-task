944a4d8f09054a8b109d21a657274700d07a9b6a
mikakost <mikayel.g.kostanyan@gmail.com>
2021-11-30 12:50:04 +0400

    grouping tests in test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,16 +1,28 @@
-import time
+import pytest
+from pages.login_page import LoginPage
 from pages.main_page import MainPage
 from pages.product_page import ProductPage
 
-'''def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()'''
+
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
+    # не забываем передать первым аргументом self
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 
+@pytest.mark.skip
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
@@ -20,6 +32,7 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     page.is_basket_empty_message_appear()
 
 
+@pytest.mark.skip
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/excession_51/"
     page = ProductPage(browser, link)
